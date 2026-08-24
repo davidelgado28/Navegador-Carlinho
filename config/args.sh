4174e28857d122ba1f47d995560d67d959f93e27
David Carlos <davidcarlosdelgado28@gmail.com>
2026-08-24 20:48:49 -0300

    Update args.sh

--- config/args.sh
+++ config/args.sh
@@ -1,1 +1,24 @@
+# Configurações de compilação para o Chromium (Hardening & Privacidade)
 
+is_debug = false
+is_official_build = true
+is_component_build = false
+symbol_level = 0
+
+google_api_key = ""
+google_default_client_id = ""
+google_default_client_secret = ""
+enable_hangout_services_extension = false
+enable_mdns = false
+enable_reading_list = false
+enable_remoting = false
+enable_reporting = false
+enable_service_discovery = false
+enable_telemetry = false
+
+# Forçar DNS sobre HTTPS 
+enable_dns_over_https = true
+
+# Criptografia e Segurança
+use_nss_certs = true
+use_sysroot = true
