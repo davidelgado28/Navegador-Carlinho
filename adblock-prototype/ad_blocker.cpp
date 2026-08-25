10a78597f02fc0fdca1f3033a38d40df9534b37d
David Carlos <davidcarlosdelgado28@gmail.com>
2026-08-24 21:15:03 -0300

    Refactor AdBlocker to support wildcard rules

--- adblock-prototype/ad_blocker.cpp
+++ adblock-prototype/ad_blocker.cpp
@@ -1,51 +1,70 @@
 #include "ad_blocker.h"
-#include <algorithm>
 #include <iostream>
 
 AdBlocker::AdBlocker() {}
 
-void AdBlocker::AddRule(const std::string& domain) {
-    blocked_domains_.insert(domain);
+void AdBlocker::AddRule(const std::string& pattern) {
+    bool has_wildcard = (pattern.find('*') != std::string::npos);
+    rules_.push_back({pattern, has_wildcard});
 }
 
 void AdBlocker::LoadDefaultFilters() {
-  
     std::vector<std::string> default_rules = {
-        "ads.g.doubleclick.net",
-        "analytics.google.com",
-        "trackers.facebook.com",
-        "adserver.yahoo.com",
-        "telemetry.malicious-tracker.io"
+        "ads.g.doubleclick.net",         
+        "*.doubleclick.net/*",           
+        "*analytics*",                   
+        "https://trackers.facebook.com/*" 
     };
 
-    for (const auto& domain : default_rules) {
-        AddRule(domain);
+    for (const auto& rule : default_rules) {
+        AddRule(rule);
     }
 }
 
-std::string AdBlocker::ExtractDomain(const std::string& url) const {
+bool AdBlocker::MatchWildcard(const std::string& pattern, const std::string& text) const {
+    size_t p_len = pattern.length();
+    size_t t_len = text.length();
+    size_t p_idx = 0, t_idx = 0;
+    size_t star_idx = std::string::npos;
+    size_t match_idx = 0;
 
-    size_t start = url.find("://");
-    if (start == std::string::npos) {
-        start = 0;
-    } else {
-        start += 3;
+    while (t_idx < t_len) {
+        if (p_idx < p_len && (pattern[p_idx] == text[t_idx] || pattern[p_idx] == '*')) {
+            if (pattern[p_idx] == '*') {
+                star_idx = p_idx;
+                match_idx = t_idx;
+                p_idx++;
+            } else {
+                p_idx++;
+                t_idx++;
+            }
+        } else if (star_idx != std::string::npos) {
+            p_idx = star_idx + 1;
+            match_idx++;
+            t_idx = match_idx;
+        } else {
+            return false;
+        }
     }
 
-    size_t end = url.find('/', start);
-    if (end == std::string::npos) {
-        end = url.length();
+    while (p_idx < p_len && pattern[p_idx] == '*') {
+        p_idx++;
     }
 
-    return url.substr(start, end - start);
+    return p_idx == p_len;
 }
 
 bool AdBlocker::ShouldBlock(const std::string& url) const {
-    std::string domain = ExtractDomain(url);
-
-    if (blocked_domains_.find(domain) != blocked_domains_.end()) {
-        return true;
+    for (const auto& rule : rules_) {
+        if (rule.is_wildcard) {
+            if (MatchWildcard(rule.pattern, url)) {
+                return true;
+            }
+        } else {
+            if (url.find(rule.pattern) != std::string::npos) {
+                return true;
+            }
+        }
     }
-
     return false;
 }
