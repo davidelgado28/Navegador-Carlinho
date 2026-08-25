c3034ed4b3841830c4895df7981cb8f42a9fc1fa
David Carlos <davidcarlosdelgado28@gmail.com>
2026-08-24 21:04:25 -0300

    Update ad_blocker.h

--- adblock-prototype/ad_blocker.h
+++ adblock-prototype/ad_blocker.h
@@ -1,1 +1,22 @@
+#ifndef AD_BLOCKER_H
+#define AD_BLOCKER_H
 
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+class AdBlocker {
+public:
+    AdBlocker();
+    ~AdBlocker() = default;
+
+    void AddRule(const std::string& domain);
+    void LoadDefaultFilters();
+    bool ShouldBlock(const std::string& url) const;
+
+private:
+    std::unordered_set<std::string> blocked_domains_;
+    std::string ExtractDomain(const std::string& url) const;
+};
+
+#endif 
